757357d8e523f679dc9be55d4e607d400fb6b0a2
Dell <muskan.mittal_cs18@gla.ac.in>
2020-03-28 12:40:51 +0530

    init commit

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -5,6 +5,56 @@
  *  Time: 7:17 PM
  */
 package problem1.mybst;
+
+import problem1.node.TreeNode;
+
 // to implement BinarySearchTree
+
 public class MyBinarySearchTree {
+    public static StringBuilder preOrderString;
+    static int levels = 0;
+    public TreeNode root;
+
+    public MyBinarySearchTree() {
+        this.root = null;
+        preOrderString = new StringBuilder();
+    }
+
+    public void add(final int value) {
+        this.root = addService(this.root, value);
+    }
+
+    public TreeNode addService(final TreeNode treeRoot, final int addingValue) {
+        if (treeRoot == null) {
+            return new TreeNode(addingValue);
+        }
+        if (addingValue < treeRoot.getKey()) {
+            treeRoot.setLeft(addService(treeRoot.getLeft(), addingValue));
+        } else if (addingValue == treeRoot.getKey()) {
+            return treeRoot;
+        } else {
+            treeRoot.setRight(addService(treeRoot.getRight(), addingValue));
+        }
+
+        return treeRoot;
+    }
+    public static void preOrder(TreeNode root){
+        if(root == null)
+        {
+            return ;
+        }
+        preOrderString.append(root.getKey()+" ");
+        preOrder(root.getLeft());
+        preOrder(root.getRight());
+    }
+
+    public void postOrder(TreeNode root){
+        if(root == null)
+        {
+            return ;
+        }
+        postOrder(root.getLeft());
+        postOrder(root.getRight());
+        System.out.print(root.getKey()+" ");
+    }
 }
--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -7,4 +7,35 @@
 package problem1.node;
 
 public class TreeNode {
+    private int key;
+    private TreeNode left, right;
+
+    public TreeNode(int item) {
+        key = item;
+        left = right = null;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+    public TreeNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(TreeNode left) {
+        this.left = left;
+    }
+
+    public TreeNode getRight() {
+        return right;
+    }
+
+    public void setRight(TreeNode right) {
+        this.right = right;
+    }
 }
